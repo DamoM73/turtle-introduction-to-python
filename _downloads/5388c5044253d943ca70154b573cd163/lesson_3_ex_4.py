03a854572dcb8fee5e43c90a7ee02bd3667dc7fd
GitHub Action <action@github.com>
2023-03-09 06:59:27 +0000

    deploy

---
+++ _downloads/5388c5044253d943ca70154b573cd163/lesson_3_ex_4.py
@@ -0,0 +1,15 @@
+import turtle
+
+# set up screen
+screen = 500
+window = turtle.Screen()
+window.setup(screen, screen)
+
+# create turtle instance
+my_ttl = turtle.Turtle()
+my_ttl.shape("arrow")
+
+###################################
+## Using the turtle commands you ##
+## have learnt, draw a house.    ##
+###################################
