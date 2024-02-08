ade63d1b729d0abe7dc0e1de8abb3e8f3396f708
GitHub Action <action@github.com>
2024-02-08 04:09:10 +0000

    deploy

---
+++ _downloads/31a6ef53ac0d32cdaaff2686c30998b4/lesson_5_ex_2.py
@@ -0,0 +1,10 @@
+# Amy's security guard program
+
+friends = "Bruce"
+
+######################################################
+## Write a program that asks for a person's name    ##
+## and then grants entry of that person is Amy      ##
+## or a friend of Amy.                              ##
+## Everyone else is told, politely, to go away      ##
+######################################################
---
+++ _downloads/5388c5044253d943ca70154b573cd163/lesson_3_ex_4.py
@@ -0,0 +1,16 @@
+import turtle
+
+# set up screen
+screen = 500
+window = turtle.Screen()
+window.setup(screen, screen)
+
+# create turtle instance
+my_ttl = turtle.Turtle()
+my_ttl.shape("arrow")
+
+###################################
+## Using the turtle commands you ##
+## have learnt, draw a house     ##
+## made up of several shapes.    ##
+###################################
